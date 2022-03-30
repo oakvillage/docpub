ff72023887afa4bf063bf3f107e2dd6a66aed3af
kkouji <kimurazaka+blog@gmail.com>
2022-03-30 23:04:09 +0900

    add schema.json

--- commit.py
+++ commit.py
@@ -1,7 +1,10 @@
 import sys, json
 
-args = sys.argv
+def output_files(file_info):
+    print(json.dumps(file_info))
 
-contents = args[1]
-#fileInfo = json.loads(contents)
-print(contents)
+if __name__ == '__main__':
+    args = sys.argv
+    raw_file_info = args[1]
+    file_info = json.loads(raw_file_info)
+    output_files(file_info)
